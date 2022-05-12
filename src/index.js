0761b8cf2daa63525a2f3b05aa72155a9bcfcdcf
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-05-11 18:56:17 -0700

    add all files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+// import Triangle from './js/triangle.js';
+
+// $(function () {
+//   $('#triangle-checker-form').on("submit", function (event) {
+//     event.preventDefault();
+//     console.log('hello');
+//     const length1 = parseInt($('#length1').val());
+//     const length2 = parseInt($('#length2').val());
+//     const length3 = parseInt($('#length3').val());
+//     const triangle = new Triangle(length1, length2, length3);
+//     const response = triangle.checkType();
+//     $('#response').append(`<p>${response}</p>`);
+//   });
+
